e2467c6bc5d5f79b559103abc017d15680b9947d
nyanchan <personal@nyanchangames.com>
2021-02-17 22:46:58 +0900

    Add Question function to nyancode runtime

--- nyancode_runtime/__init__.py
+++ nyancode_runtime/__init__.py
@@ -10,6 +10,12 @@ import time
 def message(title, message):
     ctypes.windll.user32.MessageBoxW(0, message, title, 0x00000040)
 
+# 質問ダイアログを表示
+
+
+def question(title, message):
+    return ctypes.windll.user32.MessageBoxW(0, message, title, 0x00000004) == 6
+
 # 一定時間待つ
 
 
