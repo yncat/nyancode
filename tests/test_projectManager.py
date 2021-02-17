3cf3ff0d229ff99b159344312c69f808e7098390
nyanchan <personal@nyanchangames.com>
2021-02-17 21:23:53 +0900

    add 3 tests

--- tests/test_projectManager.py
+++ tests/test_projectManager.py
@@ -6,6 +6,17 @@ import project
 import node
 
 
+def testProjectNode():
+    r = node.new("RootNode")
+    blk = block.Block()
+    n = node.new("MessageNode")
+    n.setSingleParameter("title", "title")
+    n.setSingleParameter("message", "message")
+    blk.insert(n)
+    r.setSingleChildBlock("block", blk)
+    return r
+
+
 class TestProjectManager(unittest.TestCase):
     def test_new(self):
         m = project.Manager(logger=None)
@@ -20,3 +31,43 @@ class TestProjectManager(unittest.TestCase):
         self.assertEqual("new project", m.project_name)  # プロジェクト名が設定されているか
         self.assertEqual("", m.project_path)  # 保存してないので、パスはまだ設定されてない
         self.assertFalse(m.has_changes)
+
+    def test_getList(self):
+        n = testProjectNode()
+        m = project.Manager(logger=None)
+        m.root_node = n
+        m.browsing_block = n.child_blocks["block"]
+        want = [
+            ("メッセージを表示", 2, 0)
+        ]
+        self.assertEqual(want, m.getList())
+
+    def test_insertNodeToCurrentBlock(self):
+        n = testProjectNode()
+        m = project.Manager(logger=None)
+        m.has_changes = False
+        m.root_node = n
+        m.browsing_block = n.child_blocks["block"]
+        n2 = node.new("MessageNode")
+        n2.setSingleParameter("title", "title2")
+        n2.setSingleParameter("message", "message2")
+        m.insertNodeToCurrentBlock(n2)
+        self.assertEqual(2, len(n.child_blocks["block"].nodes))
+        want = [
+            ("メッセージを表示", 2, 0),
+            ("メッセージを表示", 2, 0)
+        ]
+        self.assertEqual(want, m.getList())
+        self.assertTrue(m.has_changes)
+
+    def test_deleteMultipleNodes(self):
+        n = testProjectNode()
+        m = project.Manager(logger=None)
+        m.has_changes = False
+        m.root_node = n
+        m.browsing_block = n.child_blocks["block"]
+        m.deleteMultipleNodes([0])
+        self.assertEqual(0, len(n.child_blocks["block"].nodes))
+        want = []
+        self.assertEqual(want, m.getList())
+        self.assertTrue(m.has_changes)
