13a230f63043804c23021876468930338674b802
nyanchan <personal@nyanchangames.com>
2021-02-28 13:59:14 +0900

    Sound works

--- makefile
+++ makefile
@@ -4,13 +4,15 @@ run:
 run-sample1:
 	py nyancode.py samples/sample1.ncp
 
+run-sample2:
+	py nyancode.py samples/sample2.ncp
+
 setup:
 	py -m pip install -r requirements.txt
 	cp github_hooks/pre-commit ./.git/hooks
 
 fmt:
 	py -m autopep8 -r -i -a -a .
-	py -m autoflake -r -i .
 
 test:
 	py -m unittest discover tests
--- project/runner.py
+++ project/runner.py
@@ -1,11 +1,15 @@
-import nyancode_runtime_std
+import os
+import nyancode_runtime_std as nyancode_runtime
+import constants
 
 
 class Runner:
     def __init__(self, program, parent_window):
         self.program = program
-        print(program)
         self.parent_window = parent_window
 
     def run(self):
+        nyancode_runtime.configure(
+            data_directory=constants.DATA_DIRECTORY
+        )
         exec(self.program, {"nyancode": nyancode_runtime})
